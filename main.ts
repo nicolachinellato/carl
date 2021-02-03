d4a070b7e23ab8dbb74522a1b1c79e764cc33b42
nicolachinellato <78468160+nicolachinellato@users.noreply.github.com>
2021-02-03 08:57:01 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,114 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    shimmia,
+    [img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e f c . . . . 
+        . f d d d d e e e f f . . . . . 
+        . . f f f f f e e e e f . . . . 
+        . . . . f f e e e e e e f . f f 
+        . . . f e e f e e f e e f . e f 
+        . . f e e f e e f e e e f . e f 
+        . f b d f d b f b b f e f f e f 
+        . f d d f d d f d d b e f f f f 
+        . . f f f f f f f f f f f f f . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e f c . . . . 
+        . f d d d d e e e f f . . . . . 
+        . . f e e e f f e e e f . . . . 
+        . . f f f f f e e e e e f . f f 
+        . . f d b f e e f f e e f . e f 
+        . f f d d f e f f e e e f . e f 
+        . f f f f f f e b b f e f f e f 
+        . f d d f e e e d d b e f f f f 
+        . . f f f f f f f f f f f f f . 
+        `,img`
+        . . . . . f f f f f . . . . . . 
+        . . . . f e e e e e f . . . . . 
+        . . . f d d d d d e e f . . . . 
+        . . f f f d d f f d e f f . . . 
+        . c d d e e d d d d e d d f . . 
+        . c c d d d d c d d e d f f f . 
+        . c d c c c c d d d e d f b d f 
+        . . c d d d d d d e e f f d d f 
+        . . . c d d d d e e f f e f f f 
+        . . . . f f f e e f e e e f . . 
+        . . . . f e e e e e e e f f f . 
+        . . . f e e e e e e f f f e f . 
+        . . f f e e e e f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f e e f d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . . f f f f f . . . . . . 
+        . . . . f e e e e e f . . . . . 
+        . . . f d d d d d d e f . . . . 
+        . . f d f f d d f f d f f . . . 
+        . c d d d e e d d d d e d f . . 
+        . c d c d d d d c d d e f f . . 
+        . c d d c c c c d d d e f f f f 
+        . . c d d d d d d d e f f b d f 
+        . . . c d d d d e e f f f d d f 
+        . . . . f f f e e f e e e f f f 
+        . . . . f e e e e e e e f f f . 
+        . . . f e e e e e e f f f e f . 
+        . . f f e e e e f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f f f f d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . . f d d d d d e e f f . . . . 
+        . c d d d f f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c f f d d c d d e e b d c . . . 
+        f d d f e f f f e e e f . . . . 
+        f d d f e e e f f f f f . . . . 
+        f f f f f e e e e e f f . f f . 
+        . f f f e f f e e e f f . e f . 
+        . f b d f e f f b b f f f e f . 
+        . f d d f e e f d d b f f e f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . . f d d f d d e e f f . . . . 
+        . c d d d f d d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c f f f f d d d e e f c f . . . 
+        . f b d f f f e e e e f . . . . 
+        . f d d f e f f f e e f . . . . 
+        . . f f f e e e e f f f . f f . 
+        . . f e e f e e e e f f . e f . 
+        . f f e e e f f f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f f e e d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `],
+    200,
+    true
+    )
+})
+let shimmia: Sprite = null
 scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
@@ -138,7 +249,7 @@ let arancia = sprites.create(img`
     . . . . c c b 4 4 4 b b 4 5 4 4 
     . . . . . . c c c c c c b b 4 . 
     `, SpriteKind.Projectile)
-let shimmia = sprites.create(img`
+shimmia = sprites.create(img`
     . . . . f f f f f . . . . . . . 
     . . . f e e e e e f . . . . . . 
     . . f d d d d e e e f . . . . . 
